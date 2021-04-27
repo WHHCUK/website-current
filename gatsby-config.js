e09e5157a0d8cf3277b70a5b2f4954b6abe20b93
Jared Phayer <dev@jared.ph>
2021-04-27 01:52:44 +0100

    chore: add webpack bundle analyser

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,16 +2,22 @@ require('dotenv').config({
   path: '.env',
 });
 
-module.exports = {
-  plugins: [
-    {
-      resolve: 'gatsby-source-contentful',
-      options: {
-        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
-        spaceId: process.env.CONTENTFUL_SPACE_ID,
-      },
+const plugins = [
+  {
+    resolve: 'gatsby-source-contentful',
+    options: {
+      accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      spaceId: process.env.CONTENTFUL_SPACE_ID,
     },
-    'gatsby-transformer-sharp',
-    'gatsby-plugin-sharp',
-  ],
+  },
+  'gatsby-transformer-sharp',
+  'gatsby-plugin-sharp',
+];
+
+if (process.env.ANALYSE_WEBPACK_BUNDLES === 'true') {
+  plugins.push('gatsby-plugin-webpack-bundle-analyser-v2');
+}
+
+module.exports = {
+  plugins,
 };
